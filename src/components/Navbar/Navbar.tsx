e3ef9d2516130954f800e3e4f38c8a3f29bdcc7d
Henrique S. Souza <henrique_eduardo_souza@hotmail.com>
2024-06-16 21:48:50 -0300

    feat: update navbar items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,11 +7,12 @@ function Navbar() {
     <nav className="navbar">
       <div className="container">
         <ul>
-          <li><a>Quem Somos</a></li>
+          <li><a>Quem Somos?</a></li>
           <li><a>Nossa História</a></li>
-          <li><a>Participe do CAUSP!</a></li>
-          <li><a>Blog</a></li>
-          <li><a>Cartilhas</a></li>
+          <li><a>Descubra</a></li>
+          <li><a>Cultura</a></li>
+          <li><a>Participe</a></li>
+          <li><a>Siga-nos!</a></li>
         </ul>
       </div>
     </nav>
