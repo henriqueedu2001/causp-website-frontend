d313503b322d6efa1b1479db458d10b90dbb3b45
Henrique S. Souza <henrique_eduardo_souza@hotmail.com>
2024-06-16 23:08:46 -0300

    feat: add Home presentation content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import CAUSP_LOGO from './../../assets/imgs/LOGO SEM FUNDO.png'
 import Header from './../../components/Navbar/Navbar.tsx'
 
 import './Home.css'
@@ -7,9 +9,28 @@ function Home() {
   return (
     <>
       <Header/>
-      <h1>Coletivo Autista da USP</h1>
+      <Presentation/>
     </>
   )
+}
+
+class Presentation extends React.Component {
+  render() {
+    return (
+      <section className='presentation-section'>
+        <div className='presentation-container'>
+          <div className='logo-container'>
+            <img src={CAUSP_LOGO}></img>
+          </div>
+          <div className='text-container'>
+            <h1>Coletivo Autista<br/>da USP</h1>
+            <h2>Um espaço de conexão de pessoas autistas da comunidade USP!</h2>
+            <p>Nosso coletivo é um grupo de pessoas autistas que procuram criar amizades e compartilhar experiências e ao mesmo tempo ampliar as medidas de inclusão e adaptação no ambiente acadêmico.</p>
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default Home
