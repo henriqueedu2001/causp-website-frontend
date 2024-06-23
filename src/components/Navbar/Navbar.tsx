8a5a5646c2a120b26c3c6f27430b2e71d5cc6d9b
Henrique S. Souza <henrique_eduardo_souza@hotmail.com>
2024-06-22 22:16:19 -0300

    feat: update navbar links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
 
   return (
     <nav className="navbar">
       <div className="container">
+        <div className='causp-container'>
+          <Link to='/'>CAUSP</Link>
+        </div>
         <ul>
-          <li><a>QUEM SOMOS?</a></li>
-          <li><a>NOSSA HISTÓRIA</a></li>
-          <li><a>DESCUBRA</a></li>
-          <li><a>CULTURA</a></li>
-          <li><a>PARTICIPE</a></li>
-          <li><a>SIGA-NOS!</a></li>
+          <li><Link to='/sobrenos'>QUEM SOMOS?</Link></li>
+          <li><Link to='/historia'>NOSSA HISTÓRIA</Link></li>
+          <li><Link to='/descubra'>DESCUBRA</Link></li>
+          <li><Link to='/cultura'>CULTURA</Link></li>
+          <li><Link to='/participe'>PARTICIPE</Link></li>
+          <li><Link to='/contatos'>SIGA-NOS!</Link></li>
         </ul>
       </div>
     </nav>
